077bc221c8a1bba415f5677094bee29fa4190bf5
junc <junc.v02@gmail.com>
2025-04-20 23:34:59 -0700

    implement file_getblock

--- filesystem/unixv6/file.c
+++ filesystem/unixv6/file.c
@@ -7,6 +7,22 @@
 
 // remove the placeholder implementation and replace with your own
 int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *buf) {
-  fprintf(stderr, "file_getblock(inumber = %d, blockNum = %d) unimplemented. returning -1\n", inumber, blockNum);
-  return -1;  
+  // fprintf(stderr, "file_getblock(inumber = %d, blockNum = %d) unimplemented. returning -1\n", inumber, blockNum);
+  if (inumber < 0 || inumber >= fs->num_inodes) {
+    fprintf(stderr, "Invalid inode number %d\n", inumber);
+  }
+
+  struct inode in;
+  int err = inode_iget(fs, inumber, &in);
+  if (err < 0) {
+    return err;
+  }
+  // TODO : check the type of diskBlockNum
+  int diskBlockNum = inode_indexlookup(fs, &in, blockNum);
+  if (diskBlockNum < 0) {
+    return -1;
+  }
+
+  int readbytes = diskimg_readsector(fs->dfd, diskBlockNum, buf);
+  return readbytes;  
 }
